e574e556a16ff09b040c61e7fe727ec2fb309153
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-02-21 16:46:32 +0000

    SQL added

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- Create or use the database if it already exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or use the user if it already exists
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the user for the specified database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Ensure any existing privileges are flushed and applied
+FLUSH PRIVILEGES;
