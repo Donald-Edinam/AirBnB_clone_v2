0b89d54e84e0b4b795ed55affa85bc7ba7e8c5f7
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-03-08 13:21:01 +0000

    Project file swapped

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,12 @@
--- Create or use the database if it already exists
+-- this script prepares a MySQL server for the project
+-- create project developement database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create or use the user if it already exists
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant privileges to the user for the specified database
+-- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on performance_schema database
+FLUSH PRIVILEGES;
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Ensure any existing privileges are flushed and applied
 FLUSH PRIVILEGES;
